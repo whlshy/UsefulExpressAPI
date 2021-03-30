5e5eea16a73bf89879ce20872ec06afbd133e8ff
盧韋宏 <whl@wke.csie.ncnu.edu.tw>
2021-03-30 13:34:55 +0800

    add npm swagger-schemagen

--- swagger.js
+++ swagger.js
@@ -15,11 +15,11 @@ const doc = {
 const outputFile = './src/swagger/swagger-output.json'
 const endpointsFiles = ['./index.js']
 
-const swaggerschema = require('./src/lib/swaggerschema')
+const swaggerschema = require('swagger-schemagen')
 
 const asyncfun = async (outputFile, endpointsFiles, doc, schemaFolder) => {
     await swaggerAutogen(outputFile, endpointsFiles, doc)
-    await swaggerschema(path.resolve(outputFile), path.resolve(schemaFolder))
+    await swaggerschema(outputFile, schemaFolder)
 }
 
 const schemaFolder = './src/schema/'
